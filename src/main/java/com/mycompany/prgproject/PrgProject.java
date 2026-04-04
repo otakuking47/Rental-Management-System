be6a2fd977bed2f446e97b531dca6da2edb1faf4
CodeNewbie <amutenyajusty@gmail.com>
2026-04-04 19:19:12 +0200

    Property class code

---
+++ src/main/java/com/mycompany/prgproject/PrgProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prgproject;
+
+/**
+ *
+ * @author amute
+ */
+public class PrgProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
